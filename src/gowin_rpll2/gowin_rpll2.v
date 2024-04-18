c03cf40d1e26cd9a503eb54fcb99f18f79d358ef
Bas Visscher <bas.visscher24@gmail.com>
2024-04-18 18:22:34 +0200

    Phase delay works

--- src/gowin_rpll2/gowin_rpll2.v
+++ src/gowin_rpll2/gowin_rpll2.v
@@ -16,8 +16,10 @@ input clkin;
 wire lock_o;
 wire clkoutd_o;
 wire clkoutd3_o;
+wire gw_vcc;
 wire gw_gnd;
 
+assign gw_vcc = 1'b1;
 assign gw_gnd = 1'b0;
 
 rPLL rpll_inst (
@@ -35,26 +37,29 @@ rPLL rpll_inst (
     .ODSEL({gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd}),
     .PSDA({gw_gnd,gw_gnd,gw_gnd,gw_gnd}),
     .DUTYDA({gw_gnd,gw_gnd,gw_gnd,gw_gnd}),
-    .FDLY({gw_gnd,gw_gnd,gw_gnd,gw_gnd})
+    .FDLY({gw_vcc,gw_vcc,gw_vcc,gw_vcc})
 );
 
 defparam rpll_inst.FCLKIN = "74.25";
 defparam rpll_inst.DYN_IDIV_SEL = "false";
-defparam rpll_inst.IDIV_SEL = 0;
-defparam rpll_inst.DYN_FBDIV_SEL = "false";
+
 defparam rpll_inst.FBDIV_SEL = 0;
-defparam rpll_inst.DYN_ODIV_SEL = "false";
+defparam rpll_inst.IDIV_SEL = 0;
 defparam rpll_inst.ODIV_SEL = 8;
-defparam rpll_inst.PSDA_SEL = "0000";
-defparam rpll_inst.DYN_DA_EN = "true";
+
+
+defparam rpll_inst.DYN_FBDIV_SEL = "false";
+defparam rpll_inst.DYN_ODIV_SEL = "false";
+defparam rpll_inst.PSDA_SEL = "0100";
+defparam rpll_inst.DYN_DA_EN = "false";
 defparam rpll_inst.DUTYDA_SEL = "1000";
 defparam rpll_inst.CLKOUT_FT_DIR = 1'b1;
 defparam rpll_inst.CLKOUTP_FT_DIR = 1'b1;
 defparam rpll_inst.CLKOUT_DLY_STEP = 0;
 defparam rpll_inst.CLKOUTP_DLY_STEP = 0;
 defparam rpll_inst.CLKFB_SEL = "internal";
-defparam rpll_inst.CLKOUT_BYPASS = "true";
-defparam rpll_inst.CLKOUTP_BYPASS = "true";
+defparam rpll_inst.CLKOUT_BYPASS = "false";
+defparam rpll_inst.CLKOUTP_BYPASS = "false";
 defparam rpll_inst.CLKOUTD_BYPASS = "false";
 defparam rpll_inst.DYN_SDIV_SEL = 2;
 defparam rpll_inst.CLKOUTD_SRC = "CLKOUT";
